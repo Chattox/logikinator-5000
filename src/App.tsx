2660958cf5e1d0dc16088b342d39f540a42fe373
Chattox <conradlineker@gmail.com>
2024-06-16 17:54:08 +0100

    wip: main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { MainContainer } from "./components/MainContainer";
 
 function App() {
   return (
     <MantineProvider>
-      <p>Hello world!</p>
+      <MainContainer />
     </MantineProvider>
   );
 }
---
+++ src/components/MainContainer/MainContainer.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@mantine/core";
+
+export const MainContainer = () => {
+  return (
+    <Container>
+      <p>Hello world!</p>
+    </Container>
+  );
+};
