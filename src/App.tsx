4946895f1465cda28185d1b340844a8b4a2c37cb
Chattox <conradlineker@gmail.com>
2024-06-16 13:23:25 +0100

    build: install mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
 function App() {
-  return <p>Hello world!</p>;
+  return (
+    <MantineProvider>
+      <p>Hello world!</p>
+    </MantineProvider>
+  );
 }
 
 export default App;
